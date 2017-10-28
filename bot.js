723a0c2921384829206ca4f9076571dc93cf35b2
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2017-10-28 16:30:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,8 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('P-Pong!');
   	}
-    if (message.content === 'test') {
-    	message.reply('P-Pong!');
+    if (message.content === 'quote') {
+    	message.reply('A-Ah... I really miss the c-children back at the o-orphanage! How l-long has it been since I have seen t-them? No m-matter, I will never f-forget them! I-I must keep trying my best to see their smiling faces once m-more!');
   	}
 });
 
