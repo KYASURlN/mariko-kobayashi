8d62b9adfab10c7806d6e75e4245e4553746124a
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2017-10-28 16:11:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('P-Pong!!');
+    	message.reply('UR GOING TO BREAK ME U FUCKER');
   	}
 });
 
