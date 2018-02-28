c3a0af350eb2916cff56c4b66b81b2c077a2b197
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-02-28 17:44:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,12 +6,14 @@ client.on('ready', () => {
 });
 
 const MessageHandler = require('discord-message-handler');
-client.on('message', message => {
-    MessageHandler.whenMessageContainsWord("shrug").reply("¯\\_(ツ)_/¯");
-    MessageHandler.whenMessageContains("lol").replySometimes("kek", 50);
-    MessageHandler.whenMessageContainsExact("dota").replyOne(["volvo pls", "rip doto"]);
-    MessageHandler.whenMessageContainsOne(["br", "brazil"]).reply("huehue");
-    MessageHandler.whenMessageStartsWith("help").then(message => doSomething(message));
+    client.on('message', message => {
+    MessageHandler.handleMessage(message);
+        MessageHandler.whenMessageContainsWord("shrug").reply("¯\\_(ツ)_/¯");
+            MessageHandler.whenMessageContains("lol").replySometimes("kek", 50);
+            MessageHandler.whenMessageContainsExact("dota").replyOne(["volvo pls", "rip doto"]);
+            MessageHandler.whenMessageContainsOne(["br", "brazil"]).reply("huehue");
+            MessageHandler.whenMessageStartsWith("help").then(message => doSomething(message));
+});
 });
 
 // THIS  MUST  BE  THIS  WAY
