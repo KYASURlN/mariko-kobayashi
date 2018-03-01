816b91a82f581f46657a90a0ebece323b39e53c3
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-03-01 15:40:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,29 +5,6 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-  startTyping(count) {
-    if (typeof count !== 'undefined' && count < 1) throw new RangeError('Count must be at least 1.');
-    if (this.client.user._typing.has(this.id)) {
-      const entry = this.client.user._typing.get(this.id);
-      entry.count = count || entry.count + 1;
-    }
-
-    const entry = {
-      count: count || 3,
-      interval: this.client.setInterval(() => {
-        this.client.rest.methods.sendTyping(this.id).catch(() => {
-          this.client.clearInterval(entry.interval);
-          this.client.user._typing.delete(this.id);
-        });
-      }, 9000),
-    };
-    this.client.rest.methods.sendTyping(this.id).catch(() => {
-      this.client.clearInterval(entry.interval);
-      this.client.user._typing.delete(this.id);
-    });
-    this.client.user._typing.set(this.id, entry);
-  }
-
 client.on('message', message => {
     if (message.content.toLowerCase() === 'ping') {
         message.channel.sendMessage('Ding Dong!');
@@ -39,17 +16,6 @@ client.on('message', message => {
         message.channel.sendMessage("You're my favorite~");
         }
 });
-
-  stopTyping(force = false) {
-    if (this.client.user._typing.has(this.id)) {
-      const entry = this.client.user._typing.get(this.id);
-      entry.count--;
-      if (entry.count <= 0 || force) {
-        this.client.clearInterval(entry.interval);
-        this.client.user._typing.delete(this.id);
-      }
-    }
-  }
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
