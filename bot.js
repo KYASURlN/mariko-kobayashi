0198cb6577f81b98474d67475dd7ebca2909e7f9
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-02-28 20:28:46 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,9 @@ client.on('message', message => {
     if (message.content.toLowerCase() === 'tomo is gay') {
         message.channel.sendMessage('hes probably very gay');
         }
+    if (message.content.toLowerCase() === 'tomo is very gay') {
+        message.channel.sendMessage("he's probably very gay");
+        }
 });
 
 // THIS  MUST  BE  THIS  WAY
