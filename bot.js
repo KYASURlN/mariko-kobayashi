5184a90eb8aca5d30c8fbc2a3552be6b4cfba0c7
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-03-01 15:53:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,19 @@ client.on('message', message => {
         }
 });
 
+let Discord = require('discord.js');
+let bot = new Discord.Client();
+
+bot.on('ready', function() {
+	console.log(Discord.version);
+});
+
+bot.on('message', function(message) {
+	if (message.content == 'start')
+		message.channel.startTyping();
+	if (message.content == 'stop')
+		message.channel.stopTyping();
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
